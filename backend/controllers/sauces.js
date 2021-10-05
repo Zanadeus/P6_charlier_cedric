80e2be5b7f6ee6dcd271fe0035685f5169e3d1ce
Zanadeus <cedric.charlier@lilo.org>
2021-10-05 11:46:25 +0200

    Ajout fonctionnalité like/Dislike, améliorer les vérifications

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -76,3 +76,155 @@ exports.deleteSauce = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error }));
 };
+
+exports.setLike = (req, res, next) => 
+{
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce =>
+      {
+        /*_______________________DEFINITION DES VARIABLES_______________________*/
+        let userID = req.body.userId;
+        let sauceLikes = sauce.likes;
+        let sauceDislikes = sauce.dislikes; 
+        //let usersThatLike
+        if (sauce.usersLiked == '') 
+        {
+          usersThatLike = [];
+        } 
+        else 
+        {
+          usersThatLike = sauce.usersLiked.split(',');
+        };
+        //let usersThatDislike
+        if (sauce.usersDisliked == '') 
+        {
+          usersThatDislike = [];
+        } 
+        else 
+        {
+          usersThatDislike = sauce.usersDisliked.split(',');
+        };
+        
+        //variables nécessaires pour isUserRate()
+        let userFound = false;
+        let deleteUserPosition = '';
+
+        /*_______________________DEFINITION DES FONCTIONS_______________________*/
+        //On recherche si l'utilisateur a déja noté la sauce
+        function isUserRate(rateArray) 
+        {
+          for (let i = 0; i < rateArray.length; i++) 
+          {
+            const element = rateArray[i];
+            if (element == userID) 
+            {
+              userFound = true;
+              console.log(userFound);
+              deleteUserPosition = i;
+            }
+          }
+        }
+        //On met à jour les likes dans la base données
+        function updateLikes()
+        {
+          Sauce.updateOne(
+            { _id: req.params.id },
+            {
+              $set:
+              { 
+                likes: sauceLikes,
+                dislikes: sauceDislikes,
+                usersLiked: usersThatLike.join(','),
+                usersDisliked: usersThatDislike.join(',')
+              }
+            })
+            .then(() => res.status(200).json({ message: 'Mise à jour effectuée'}))
+            .catch(error => res.status(400).json({ error }));
+        };
+        //fonction nulle en cas d'incohérence avec le frontend
+        function nothingHappen() 
+        {
+          console.log("erreur de traitement de l'action demandée")
+          .then(() => res.status(400).json({ message: 'Echec mise à jour'}))
+          .catch(error => res.status(400).json({ error }));
+        }
+        //fonction de développement, on restaure la base de données des likes
+        function resetLikes()
+        {
+          Sauce.updateOne(
+            { _id: req.params.id },
+            {
+              $set:
+              { 
+                likes: `0`,
+                dislikes: `0`,
+                usersLiked: ``,
+                usersDisliked: ``
+              }
+            })
+            .then(() => res.status(200).json({ message: 'Mise à jour effectuée'}))
+            .catch(error => res.status(400).json({ error }));
+        };
+
+        /*_______________________COEUR DU LOGICIEL_______________________*/
+        switch (req.body.like)
+        {
+          case 1:
+            isUserRate(usersThatLike);
+            //On enregistre l'action de l'utilisateur
+            if (userFound == true) 
+            {
+              //nothingHappen();
+              usersThatLike.splice(deleteUserPosition, 1);
+              sauceLikes-- ;
+              updateLikes();
+            } 
+            else 
+            {
+              usersThatLike.push(userID);
+              sauceLikes++ ;
+              updateLikes();
+            }
+            break;
+          case -1:
+            isUserRate(usersThatDislike);
+            //On enregistre l'action de l'utilisateur
+            if (userFound == true) 
+            {
+              //nothingHappen();
+              usersThatDislike.splice(deleteUserPosition, 1);
+              sauceDislikes-- ;
+              updateLikes();
+            } 
+            else 
+            {
+              usersThatDislike.push(userID);
+              sauceDislikes++ ;
+              updateLikes();
+            }
+            break;
+          default:
+            console.log("la requête 0 a été sélectionnée.");
+            isUserRate(usersThatLike);
+            //On enregistre l'action de l'utilisateur
+            if (userFound == true) 
+            {
+              usersThatLike.splice(deleteUserPosition, 1);
+              sauceLikes-- ;
+              updateLikes();
+            } 
+
+            isUserRate(usersThatDislike);
+            //On enregistre l'action de l'utilisateur
+            if (userFound == true) 
+            {
+              usersThatDislike.splice(deleteUserPosition, 1);
+              sauceDislikes-- ;
+              updateLikes();
+            } 
+            
+            //resetLikes();
+            break;
+        }
+      })
+};
--- backend/roads/sauces.js
+++ backend/roads/sauces.js
@@ -12,6 +12,6 @@ router.get('/:id', auth, saucesCtrl.getOneSauce);//ok
 router.post('/', auth, multer, saucesCtrl.createSauce);//
 router.put('/:id', auth, multer, saucesCtrl.modifySauce);//?
 router.delete('/:id', auth, multer, saucesCtrl.deleteSauce);//ok
-//router.post('/:id/like', auth, multer, saucesCtrl.setLike);
+router.post('/:id/like', auth, multer, saucesCtrl.setLike);
 
 module.exports = router;
